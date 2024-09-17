6eae188b8b1bded6fc958febcf9345a8f5b67796
Leo J. <153937047+leiicamundi@users.noreply.github.com>
2024-09-17 18:34:10 +0200

    include auto repo filling

--- pre_commit_hooks/docker_update_action_readmes.sh
+++ pre_commit_hooks/docker_update_action_readmes.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-# This script generates or updates README.md files for GitHub actions located in the .github/actions directory.
-# It uses Docker to run the Node.js environment and the 'action-docs' tool for generating the documentation from
-# action YAML files.
-
-set -o pipefail
+set -euxo pipefail
 
 # due to an open bug in the node image https://github.com/nodejs/docker-node/issues/740
 # we can't map the user and group at the docker level, therefore we chown the files
 USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
+# define doc variables based on the git repository as this does not work correctly using
+# the built-in method
+REPO_URL=$(git config --get remote.origin.url)
+REPO_URL_CLEAN=$(echo "$REPO_URL" | sed 's/.git$//')
+OWNER_PROJECT=$(echo "$REPO_URL_CLEAN" | awk -F'[:/]' '{print $(NF-1)"/"$NF}')
+DOC_ACTION_VERSION=${DOC_ACTION_VERSION:-main}
+
 # Run a single Docker container to handle the README.md updates
 docker run --rm \
     -e USER_ID="$USER_ID" \
     -e GROUP_ID="$GROUP_ID" \
+    -e DOC_ACTION_VERSION="$DOC_ACTION_VERSION" \
+    -e OWNER_PROJECT="$OWNER_PROJECT" \
     -v "$PWD":/workspace \
     -w /workspace \
     node:22 \
@@ -23,11 +28,12 @@ docker run --rm \
         npm install -g action-docs
         find .github/actions -name "*.yml" -o -name "*.yaml" | while read -r action_file; do
             action_dir=$(dirname "$action_file")
+            action_dir_top=$(basename "$action_dir")
             echo "Updating README.md in $action_dir"
-            rm -f "$action_dir/README.md"
             action-docs -t 1 --no-banner -n -s "$action_file" > "$action_dir/README.md.tmp"
             # Ensure that only a single empty line is left at the end of the file
             sed -e :a -e "/^\n*\$/{\$d;N;};/\n\$/ba" "$action_dir/README.md.tmp" > "$action_dir/README.md"
+            sed -i "s|\*\*\*PROJECT\*\*\*@\*\*\*VERSION\*\*\*|$OWNER_PROJECT/$action_dir_top@$DOC_ACTION_VERSION|g" "$action_dir/README.md"
             chown "$USER_ID:$GROUP_ID" "$action_dir/README.md"
             rm -f "$action_dir/README.md.tmp"
         done
