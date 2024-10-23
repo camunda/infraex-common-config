2b3403f87a3fe4a90a40a427edccca2ba6040da6
Leo J <153937047+leiicamundi@users.noreply.github.com>
2024-10-23 15:38:53 +0200

    introduce pagination

--- .github/workflows/scripts/aws_global_cleanup.sh
+++ .github/workflows/scripts/aws_global_cleanup.sh
@@ -24,11 +24,42 @@ execute_or_simulate() {
     fi
 }
 
+# Function to paginate through AWS CLI output
+paginate() {
+    local command="$1"
+    local query="$2"
+    local output=""
+    local next_token=""
+
+    while : ; do
+        # Execute the command with the next token if it exists
+        if [ -z "$next_token" ]; then
+            output=$($command --output text --query "$query" || true)
+        else
+            output=$($command --output text --query "$query" --starting-token "$next_token" || true)
+        fi
+
+        # If output is empty, break the loop
+        if [ -z "$output" ]; then
+            break
+        fi
+
+        echo "$output"
+
+        # Get the next token from the command output
+        next_token=$($command --output text --query 'NextToken' || true)
+
+        if [ "$next_token" == "None" ] || [ -z "$next_token" ]; then
+            break
+        fi
+    done
+}
+
 echo "Deleting additional global resources..."
 
 echo "Deleting IAM Users"
 # Delete Users
-usernames=$(aws iam list-users --query "Users[].UserName" --output text --no-paginate)
+usernames=$(paginate "aws iam list-users" "Users[].UserName")
 
 if [ -n "$usernames" ]; then
     read -r -a usernames_array <<< "$usernames"
@@ -41,7 +72,8 @@ if [ -n "$usernames" ]; then
         fi
 
         echo "Processing user: $username"
-        attached_policy_arns=$(aws iam list-attached-user-policies --user-name "$username" --query 'AttachedPolicies[].PolicyArn' --output text --no-paginate)
+        attached_policy_arns=$(paginate "aws iam list-attached-user-policies --user-name $username" "AttachedPolicies[].PolicyArn")
+
         if [ -n "$attached_policy_arns" ]; then
             read -r -a attached_policy_arns_array <<< "$attached_policy_arns"
             for policy_arn in "${attached_policy_arns_array[@]}"
@@ -51,7 +83,7 @@ if [ -n "$usernames" ]; then
             done
         fi
 
-        inline_policy_names=$(aws iam list-user-policies --user-name "$username" --query 'PolicyNames' --output text  --no-paginate)
+        inline_policy_names=$(paginate "aws iam list-user-policies --user-name $username" "PolicyNames")
         if [ -n "$inline_policy_names" ]; then
             read -r -a inline_policy_names_array <<< "$inline_policy_names"
             for policy_name in "${inline_policy_names_array[@]}"
@@ -61,7 +93,7 @@ if [ -n "$usernames" ]; then
             done
         fi
 
-        access_key_ids=$(aws iam list-access-keys --user-name "$username" --query 'AccessKeyMetadata[].AccessKeyId' --output text --no-paginate)
+        access_key_ids=$(paginate "aws iam list-access-keys --user-name $username" "AccessKeyMetadata[].AccessKeyId")
         if [ -n "$access_key_ids" ]; then
             read -r -a access_key_ids_array <<< "$access_key_ids"
             for access_key_id in "${access_key_ids_array[@]}"
@@ -78,7 +110,7 @@ fi
 
 echo "Deleting IAM Roles"
 # Detach permissions and profile instances and delete IAM roles
-role_arns=$(aws iam list-roles --query "Roles[].RoleName" --output text --no-paginate)
+role_arns=$(paginate "aws iam list-roles" "Roles[].RoleName")
 
 if [ -n "$role_arns" ]; then
     read -r -a role_arns_array <<< "$role_arns"
@@ -92,7 +124,7 @@ if [ -n "$role_arns" ]; then
         fi
 
         echo "Removing instance profiles and policies of role: $role_arn"
-        attached_policy_arns=$(aws iam list-attached-role-policies --role-name "$role_arn" --query 'AttachedPolicies[].PolicyArn' --output text --no-paginate)
+        attached_policy_arns=$(paginate "aws iam list-attached-role-policies --role-name $role_arn" "AttachedPolicies[].PolicyArn")
 
         if [ -n "$attached_policy_arns" ]; then
             read -r -a attached_policy_arns_array <<< "$attached_policy_arns"
@@ -103,18 +135,19 @@ if [ -n "$role_arns" ]; then
                 execute_or_simulate "aws iam detach-role-policy --role-name $role_arn --policy-arn $policy_arn"
             done
 
-            policy_arns=$(aws iam list-role-policies --role-name "$role_arn" --query 'PolicyNames' --output text --no-paginate)
+            policy_arns=$(paginate "aws iam list-role-policies --role-name $role_arn" "PolicyNames")
+
             if [ -n "$policy_arns" ]; then
                 read -r -a policy_arns_array <<< "$policy_arns"
 
-                for policy_arn in "${policy_arns_array[@]}"
+                for policy_name in "${policy_arns_array[@]}"
                 do
-                    echo "Deleting policy: $policy_arn"
-                    execute_or_simulate "aws iam delete-role-policy --role-name $role_arn --policy-name $policy_arn"
+                    echo "Deleting policy: $policy_name"
+                    execute_or_simulate "aws iam delete-role-policy --role-name $role_arn --policy-name $policy_name"
                 done
             fi
 
-            instance_profile_arns=$(aws iam list-instance-profiles-for-role --role-name "$role_arn" --query 'InstanceProfiles[].InstanceProfileName' --output text --no-paginate)
+            instance_profile_arns=$(paginate "aws iam list-instance-profiles-for-role --role-name $role_arn" "InstanceProfiles[].InstanceProfileName")
 
             if [ -n "$instance_profile_arns" ]; then
                 read -r -a instance_profile_arns_array <<< "$instance_profile_arns"
@@ -134,14 +167,14 @@ fi
 
 echo "Deleting IAM Policies"
 # Delete Policies
-iam_policies=$(aws iam list-policies --query "Policies[].Arn" --output text --no-paginate)
+iam_policies=$(paginate "aws iam list-policies" "Policies[].Arn")
 
 if [ -n "$iam_policies" ]; then
     read -r -a iam_policies_array <<< "$iam_policies"
 
     for iam_policy in "${iam_policies_array[@]}"
     do
-        # Skip policies prefixed that we want to keep
+        # Skip policies prefixed that we veulent garder
         if [[ "$iam_policy" == AWS* || "$iam_policy" == AmazonEKS* || "$iam_policy" == ManagedOpenShift* || "$iam_policy" == OrganizationAccountAccessRole* ]]; then
             echo "Skipping policy: $iam_policy"
             continue
@@ -153,7 +186,7 @@ if [ -n "$iam_policies" ]; then
 fi
 
 echo "Deleting nightly S3 Buckets"
-bucket_ids=$(aws s3api list-buckets --query "Buckets[?contains(Name, 'nightly')].Name" --output text --no-paginate)
+bucket_ids=$(paginate "aws s3api list-buckets" "Buckets[?contains(Name, 'nightly')].Name")
 
 if [ -n "$bucket_ids" ]; then
     read -r -a buckets <<< "$bucket_ids"
@@ -169,14 +202,13 @@ if [ -n "$bucket_ids" ]; then
 fi
 
 echo "Deleting IAM Identity Providers"
-identity_providers=$(aws iam list-open-id-connect-providers --query 'OpenIDConnectProviderList[].Arn' --output text --no-paginate)
+identity_providers=$(paginate "aws iam list-open-id-connect-providers" "OpenIDConnectProviderList[].Arn")
 
 if [ -n "$identity_providers" ]; then
     read -r -a identity_providers_array <<< "$identity_providers"
 
     for provider in "${identity_providers_array[@]}"
     do
-
         if [[ "$provider" == *DO_NOT_DELETE* ]]; then
             echo "Skipping provider: $provider (marked as DO_NOT_DELETE)"
             continue
--- .github/workflows/scripts/aws_regional_cleanup.sh
+++ .github/workflows/scripts/aws_regional_cleanup.sh
@@ -14,26 +14,71 @@ region="$1"
 
 echo "Deleting additional resources in the $region region..."
 
+# Function to execute a command or simulate it if DRY_RUN is true
+execute_or_simulate() {
+    local cmd="$1"
+    if [ "$DRY_RUN" = true ]; then
+        echo "[DRY RUN] Would execute: $cmd"
+    else
+        eval "$cmd"
+    fi
+}
+
+# Function to paginate through AWS CLI output
+paginate() {
+    local command="$1"
+    local query="$2"
+    local output=""
+    local next_token=""
+
+    while : ; do
+        # Execute the command with the next token if it exists
+        if [ -z "$next_token" ]; then
+            output=$($command --output text --query "$query" || true)
+        else
+            output=$($command --output text --query "$query" --starting-token "$next_token" || true)
+        fi
+
+        # If output is empty, break the loop
+        if [ -z "$output" ]; then
+            break
+        fi
+
+        echo "$output"
+
+        # Get the next token from the command output
+        next_token=$($command --output text --query 'NextToken' || true)
+
+        if [ "$next_token" == "None" ] || [ -z "$next_token" ]; then
+            break
+        fi
+    done
+}
+
 echo "Deleting OIDC Providers"
 # Delete OIDC Provider
-oidc_providers=$(aws iam list-open-id-connect-providers --query "OpenIDConnectProviderList[?contains(Arn, '$region')].Arn" --output text --no-paginate)
+oidc_providers=$(paginate "aws iam list-open-id-connect-providers" "OpenIDConnectProviderList[?contains(Arn, '$region')].Arn")
 
-read -r -a oidc_providers_array <<< "$oidc_providers"
+if [ -n "$oidc_providers" ]; then
+    read -r -a oidc_providers_array <<< "$oidc_providers"
 
-for oidc_provider in "${oidc_providers_array[@]}"
-do
-    echo "Deleting OIDC Provider: $oidc_provider"
-    aws iam delete-open-id-connect-provider --open-id-connect-provider-arn "$oidc_provider"
-done
+    for oidc_provider in "${oidc_providers_array[@]}"
+    do
+        echo "Deleting OIDC Provider: $oidc_provider"
+        execute_or_simulate "aws iam delete-open-id-connect-provider --open-id-connect-provider-arn \"$oidc_provider\""
+    done
+fi
 
 echo "Deleting VPC Peering Connections"
 # Delete VPC Peering Connection
-peering_connection_ids=$(aws ec2 describe-vpc-peering-connections --region "$region" --query "VpcPeeringConnections[?Status.Code == 'active']".VpcPeeringConnectionId --output text --no-paginate)
+peering_connection_ids=$(paginate "aws ec2 describe-vpc-peering-connections --region \"$region\"" "VpcPeeringConnections[?Status.Code == 'active'].VpcPeeringConnectionId")
 
-read -r -a peering_connection_ids_array <<< "$peering_connection_ids"
+if [ -n "$peering_connection_ids" ]; then
+    read -r -a peering_connection_ids_array <<< "$peering_connection_ids"
 
-for peering_connection_id in "${peering_connection_ids_array[@]}"
-do
-    echo "Deleting VPC Peering Connection: $peering_connection_id"
-    aws ec2 delete-vpc-peering-connection --region "$region" --vpc-peering-connection-id "$peering_connection_id"
-done
+    for peering_connection_id in "${peering_connection_ids_array[@]}"
+    do
+        echo "Deleting VPC Peering Connection: $peering_connection_id"
+        execute_or_simulate "aws ec2 delete-vpc-peering-connection --region \"$region\" --vpc-peering-connection-id \"$peering_connection_id\""
+    done
+fi
